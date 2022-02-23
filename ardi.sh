cbbada85c5bb68ba3a05a1d4c5c845e3c428c9ed
Maf69 <74432906+Maf69@users.noreply.github.com>
2022-02-23 10:30:19 +0100

    Update ardi.sh

--- ardi.sh
+++ ardi.sh
@@ -11,24 +11,28 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
-# You can add xorg to the installation packages, I usually add it at the DE or WM install script
-# You can remove the tlp package if you are installing on a desktop or vm
 pacman -Syy
 
 
-pacman -S grub grub-btrfs btrfs-progs efibootmgr networkmanager network-manager-applet dialog 
-wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs
-xdg-utils gvfs gvfs-smb bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse 
-pipewire-jack bash-completion openssh rsync reflector tlp openbsd-netcat iptables-nft ipset 
-firewalld flatpak os-prober ntfs-3g xorg sddm plasma kde-applications kdenetwork-filesharing 
-cifs-utils powerdevil kdepim kde-gtk-config  breeze-gtk packagekit-qt5 wireless_tools
+pacman -S grub grub-btrfs btrfs-progs efibootmgr 
 
+pacnan -S networkmanager network-manager-applet dialog base-devel
 
+pacman -S wpa_supplicant mtools dosfstools reflector 
 
-# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S linux-headers avahi xdg-user-dirs pulseaudio
 
+pacman -S xdg-utils gvfs gvfs-smb bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse 
 
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S bash-completion openssh rsync ipset samba 
+
+pacman -S flatpak os-prober ntfs-3g xorg sddm plasma 
+
+pacman -S kde-applications kdenetwork-filesharing 
+
+pacman -S cifs-utils powerdevil kdepim kde-gtk-config breeze-gtk packagekit-qt5 wireless_tools
+
+pacman -S nvidia nvidia-utils nvidia-settings
 
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 
